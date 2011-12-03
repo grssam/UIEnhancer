6ed6da4f733b67a34d5fddd364d8abd8803fde4c
scrapmac <scrapmachines@gmail.com>
2011-12-04 01:53:32 +0530

    Version 2.5 pre 7

---
+++ scripts/helper.js
@@ -0,0 +1,225 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ *  This is the helper file for addon UI Enhancement
+ *  Copyright (C) 2011  Girish Sharma
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/
+ *
+ *  The original code is the helper and utils file for addon Home Dash.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+"use strict";
+
+// Helper that adds event listeners and remembers to remove on unload
+function listen(window, node, event, func, capture) {
+  // Default to use capture
+  if (capture == null)
+    capture = true;
+
+  node.addEventListener(event, func, capture);
+  function undoListen() {
+    node.removeEventListener(event, func, capture);
+  }
+
+  // Undo the listener on unload and provide a way to undo everything
+  let undoUnload = unload(undoListen, window);
+  return function() {
+    undoListen();
+    undoUnload();
+  };
+}
+
+// Async version of SpinQuery
+// Enhanced by Girish Sharma
+function spinQueryAsync(connection, {names, params, query}, {callback, args}) {
+  // Remember the results from processing the query
+  let allResults = [];
+
+  // Nothing to do with no query
+  if (query == null) {
+    args[args.length] = allResults;
+    callback.call(callback, args);
+    return;
+  }
+
+  // Create the statement and add parameters if necessary
+  let statement = connection.createAsyncStatement(query);
+  if (params != null) {
+    Object.keys(params).forEach(function(key) {
+      statement.params[key] = params[key];
+    });
+  }
+
+  // Start the query and prepare to cancel if necessary
+  let pending = statement.executeAsync({
+    // Remember that we finished successfully
+    handleCompletion: function handleCompletion(reason) {
+      if (reason != Ci.mozIStorageStatementCallback.REASON_ERROR) {
+        args[args.length] = allResults;
+        callback.call(callback, args);
+      }
+    },
+
+    // Remember that we finished with an error
+    handleError: function handleError(error) {
+      throw error;
+    },
+
+    // Process the batch of results and save them for later
+    handleResult: function handleResult(results) {
+      let row;
+      while ((row = results.getNextRow()) != null) {
+        let item = {};
+        names.forEach(function(name) {
+          item[name] = row.getResultByName(name);
+        });
+        allResults.push(item);
+      }
+    },
+  });
+}
+
+function unload(callback, container) {
+  // Initialize the array of unloaders on the first usage
+  let unloaders = unload.unloaders;
+  if (unloaders == null)
+    unloaders = unload.unloaders = [];
+
+  // Calling with no arguments runs all the unloader callbacks
+  if (callback == null) {
+    unloaders.slice().forEach(function(unloader) unloader());
+    unloaders.length = 0;
+    return;
+  }
+
+  // The callback is bound to the lifetime of the container if we have one
+  if (container != null) {
+    // Remove the unloader when the container unloads
+    container.addEventListener("unload", removeUnloader, false);
+
+    // Wrap the callback to additionally remove the unload listener
+    let origCallback = callback;
+    callback = function() {
+      container.removeEventListener("unload", removeUnloader, false);
+      origCallback();
+    }
+  }
+
+  // Wrap the callback in a function that ignores failures
+  function unloader() {
+    try {
+      callback();
+    }
+    catch(ex) {}
+  }
+  unloaders.push(unloader);
+
+  // Provide a way to remove the unloader
+  function removeUnloader() {
+    let index = unloaders.indexOf(unloader);
+    if (index != -1)
+      unloaders.splice(index, 1);
+  }
+  return removeUnloader;
+}
+
+function watchWindows(callback) {
+  var unloaded = false;
+  unload(function() unloaded = true);
+
+  // Wrap the callback in a function that ignores failures
+  function watcher(window) {
+    try {
+      // Now that the window has loaded, only handle browser windows
+      let {documentElement} = window.document;
+      if (documentElement.getAttribute("windowtype") == "navigator:browser")
+        callback(window);
+    }
+    catch(ex) {}
+  }
+
+  // Wait for the window to finish loading before running the callback
+  function runOnLoad(window) {
+    // Listen for one load event before checking the window type
+    window.addEventListener("load", function runOnce() {
+      window.removeEventListener("load", runOnce, false);
+      if (unloaded) return; // the extension has shutdown
+      watcher(window);
+    }, false);
+  }
+
+  // Add functionality to existing windows
+  let windows = Services.wm.getEnumerator(null);
+  while (windows.hasMoreElements()) {
+    // Only run the watcher immediately if the window is completely loaded
+    let window = windows.getNext();
+    if (window.document.readyState == "complete")
+      watcher(window);
+    // Wait for the window to load before continuing
+    else
+      runOnLoad(window);
+  }
+
+  // Watch for new browser windows opening then wait for it to load
+  function windowWatcher(subject, topic) {
+    if (topic == "domwindowopened")
+      runOnLoad(subject);
+  }
+  Services.ww.registerNotification(windowWatcher);
+
+  // Make sure to stop watching for windows if we're unloading
+  unload(function() Services.ww.unregisterNotification(windowWatcher));
+}
+
+// Take a window and create various helper properties and functions
+function makeWindowHelpers(window) {
+  const XUL = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
+
+  let {document, clearTimeout, gBrowser, setTimeout} = window;
+
+  // Call a function after waiting a little bit
+  function async(callback, delay) {
+    let timer = setTimeout(function() {
+      stopTimer();
+      callback();
+    }, delay);
+
+    // Provide a way to stop an active timer
+    function stopTimer() {
+      if (timer == null)
+        return;
+      clearTimeout(timer);
+      timer = null;
+      unUnload();
+    }
+
+    // Make sure to stop the timer when unloading
+    let unUnload = unload(stopTimer, window);
+
+    // Give the caller a way to cancel the timer
+    return stopTimer;
+  }
+
+  // Replace a value with another value or a function of the original value
+  function change(obj, prop, val) {
+    let orig = obj[prop];
+    obj[prop] = typeof val == "function" ? val(orig) : val;
+    unload(function() obj[prop] = orig, window);
+  }
+
+  return {
+    async: async,
+    change: change,
+  };
+}
---
+++ scripts/pref.js
@@ -0,0 +1,105 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ *  This is the helper file for addon UI Enhancement
+ *  Copyright (C) 2011  Girish Sharma
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/
+ *
+ *  The original code is the pref file for addon Speak Words.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+"use strict";
+
+function pref(key) {
+  // Cache the prefbranch after first use
+  let {branch, defaults} = pref;
+  if (branch == null)
+    branch = Services.prefs.getBranch(pref.root);
+
+  // Figure out what type of pref to fetch
+  switch (typeof defaults[key]) {
+    case "boolean":
+      return branch.getBoolPref(key);
+    case "number":
+      return branch.getIntPref(key);
+    case "string":
+      return branch.getCharPref(key);
+  }
+  return null;
+}
+
+// Set custom values for this add-on
+pref.root = "extensions.UIEnhancer.";
+pref.defaults = {
+  bringBookmarksUp: true,
+  urlBarWidth: "700",
+  animationSpeed: "normal",
+  enhanceURLBar: true,
+  removeGibberish: false,
+  installedVersion: "0.0"
+};
+
+pref.observe = function(prefs, callback) {
+  let {root} = pref;
+  function observe(subject, topic, data) {
+    // Sanity check that we have the right notification
+    if (topic != "nsPref:changed")
+      return;
+
+    // Only care about the prefs provided
+    let pref = data.slice(root.length);
+    if (prefs.indexOf(pref) == -1)
+      return;
+
+    // Trigger the callback with the changed key
+    callback(pref);
+  }
+
+  // Watch for preference changes under the root and clean up when necessary
+  Services.prefs.addObserver(root, observe, false);
+  unload(function() Services.prefs.removeObserver(root, observe));
+};
+
+// Initialize default preferences
+let (branch = Services.prefs.getDefaultBranch(pref.root)) {
+  for (let [key, val] in Iterator(pref.defaults)) {
+    switch (typeof val) {
+      case "boolean":
+        branch.setBoolPref(key, val);
+        break;
+      case "number":
+        branch.setIntPref(key, val);
+        break;
+      case "string":
+        branch.setCharPref(key, val);
+        break;
+    }
+  }
+}
+
+function setPref(k, v) {
+  let (branch = Services.prefs.getDefaultBranch(pref.root)) {
+    switch (typeof v) {
+      case "boolean":
+        branch.setBoolPref(k, v);
+        break;
+      case "number":
+        branch.setIntPref(k, v);
+        break;
+      case "string":
+        branch.setCharPref(k, v);
+        break;
+    }
+  }
+}
