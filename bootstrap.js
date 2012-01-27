007176fd7cfa28b50b5e121d73b33a3c75a0d815
Girish Sharma <scrapmachines@gmail.com>
2012-01-27 14:56:33 +0530

    Fixing loeft margin and copy issue (false positive)

--- bootstrap.js
+++ bootstrap.js
@@ -264,7 +264,9 @@ function changeUI(window) {
       enhancedURLBar.style.display = "-moz-box";
       enhancedURLBar.style.padding = "0px";
       enhancedURLBar.style.margin = "-" + window.getComputedStyle(gURLBar).paddingTop
-        + " 0px -" + window.getComputedStyle(gURLBar).paddingBottom + " -3px";
+        + " 0px -" + window.getComputedStyle(gURLBar).paddingBottom + " -"
+        + window.getComputedStyle(gURLBar).paddingLeft.replace("px", "")*1
+        + window.getComputedStyle(origIdentity).marginRight.replace("px", "")*1 + "px";
       urlBarHeight = gURLBar.boxObject.height - 2;
       enhancedURLBar.style.maxHeight = urlBarHeight + "px";
     }
@@ -1846,7 +1848,7 @@ function changeUI(window) {
       try {
         gBrowser.removeEventListener("click", hideMainPopup, false);
       } catch (ex) {}
-      copyToClipboard(enhancedURLBar.lastChild.getAttribute("url"));
+      copyToClipboard(getURI().spec);
       arrowMouseDown = siblingsShown = false;
       highlightPart(arrowedStack, false, false);
     };
